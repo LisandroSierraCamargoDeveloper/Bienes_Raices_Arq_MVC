3a20a72de22a1e7e6acf19e576f5bbbbbec850af
Lisandro <lisansierra@outlook.com>
2025-01-07 15:36:11 -0400

    fix db.js 4mx

--- seed/seeder.js
+++ seed/seeder.js
@@ -1,48 +1,48 @@
-// import {exit} from 'node:process'
-// import cateogrias from './categorias.js'
-// import precios from './precios.js'
-// import usuarios from './usuarios.js'
-// import db from  '../config/db.js'
-// import { Categoria, Precio, Usuario } from  '../models/index.js'
+import {exit} from 'node:process'
+import cateogrias from './categorias.js'
+import precios from './precios.js'
+import usuarios from './usuarios.js'
+import db from  '../config/db.js'
+import { Categoria, Precio, Usuario } from  '../models/index.js'
 
-//const importarDatos   = async () => {
-    //try{
-        //Autenticar
-       // await db.authenticate()
+const importarDatos   = async () => {
+    try{
+        Autenticar
+       await db.authenticate()
         //Generar Columnas
-       // await db.sync()
+       await db.sync()
         //Insertar Datos 
-       // await Promise.all([
-            //Categoria.bulkCreate(cateogrias),
-           // Precio.bulkCreate(precios),
-           //Usuario.bulkCreate(usuarios)
-        // ]);
+       await Promise.all([
+            Categoria.bulkCreate(cateogrias),
+           Precio.bulkCreate(precios),
+           Usuario.bulkCreate(usuarios)
+        ]);
         
       
-        //console.log("Datos importados Correctamente ");
-       // exit()
-    //}catch(error){
-        //console.log(error);
-       // exit(1)
-   // }
-//}
+        console.log("Datos importados Correctamente ");
+       exit()
+    }catch(error){
+        console.log(error);
+       exit(1)
+   }
+}
 
 
-// const eliminarDatos = async () => {
-//     try {
-//         await db.sync({force: true})
-//         console.log('Datos Eliminados Correctamente');
-//         exit()
-//     } catch (error) {
-//         console.log(error)
-//         exit(1)
-//     }
-// }
+const eliminarDatos = async () => {
+    try {
+        await db.sync({force: true})
+        console.log('Datos Eliminados Correctamente');
+        exit()
+    } catch (error) {
+        console.log(error)
+        exit(1)
+    }
+}
 
-//     if(process.argv[2] === "-i") {
-//         importarDatos();
-//     }
+    if(process.argv[2] === "-i") {
+        importarDatos();
+    }
 
-//     if(process.argv[2] === "-e") {
-//         eliminarDatos();
-//     }
+    if(process.argv[2] === "-e") {
+        eliminarDatos();
+    }
