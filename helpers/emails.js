b8d4c11b7f53496eecbf62c91b1d38f3d8b27292
Lisandro <lisansierra@outlook.com>
2025-01-07 17:01:09 -0400

    fixed email

--- helpers/emails.js
+++ helpers/emails.js
@@ -1,67 +1,65 @@
 import nodemailer from 'nodemailer'
 
-
-
-
 const emailRegistro = async (datos) => {
     const transport = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
         auth: {
             user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASSWORD
+            pass: process.env.EMAIL_PASS
         }
     });
 
-    const { email, nombre, token } = datos
-    let Binesracies = "BienesRaices";
-    //Enviar el email:
+    const { email, nombre, token } = datos
+
+    // Enviar el email
     await transport.sendMail({
-        from: Binesracies,
+        from: 'BienesRaices.com',
         to: email,
         subject: 'Confirma tu Cuenta en BienesRaices.com',
-        Text: 'Confirma tu Cuenta en BienesRaices.com',
+        text: 'Confirma tu Cuenta en BienesRaices.com',
         html: `
-            <p>hola  ${nombre} confirma tu cuenta en Bienes Raices.com</p>
-            <p> tu cuenta ya esta lista solo debes confirmarla en el siguiente enlace :
-            <a href="${process.env.BACKEND_URL}:${process.env.PORT ?? 3000}/out/confirmar/${token}">Confirmar cuenta</a>  </p>
+            <p>Hola ${nombre}, comprueba tu cuenta en bienesRaices.com</p>
 
-            <p> Si tu no creaste esta cuenta puedes ignorar el mensaje  </p>
-            `
+            <p>Tu cuenta ya esta lista, solo debes confirmarla en el siguiente enlace: 
+            <a href="${process.env.BACKEND_URL}:${process.env.PORT ?? 3000}/auth/confirmar/${token}">Confirmar Cuenta</a> </p>
+
+            <p>Si tu no creaste esta cuenta, puedes ignorar el mensaje</p>
+        `
     })
 }
 
-const emailRecuperacion = async (datos) => {
+const emailOlvidePassword = async (datos) => {
     const transport = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
         auth: {
             user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASSWORD
+            pass: process.env.EMAIL_PASS
         }
     });
 
-    const { email, nombre, token } = datos
-    let Binesracies = "BienesRaices";
+    const { email, nombre, token } = datos
+
+    // Enviar el email
     await transport.sendMail({
-        from: Binesracies,
+        from: 'BienesRaices.com',
         to: email,
         subject: 'Reestablece tu Password en BienesRaices.com',
-        Text: 'Reestablece tu Password en BienesRaices.com',
+        text: 'Reestablece tu Password en BienesRaices.com',
         html: `
-            <p>hola  ${nombre} Has Solicitado Reestablecer tus passwoed en  Bienes Raices.com</p>
+            <p>Hola ${nombre}, has solicitado reestablecer tu password en bienesRaices.com</p>
 
-            <p> Sigue el Siguiente Enlace para Generar tu nuevo Password  :
-            <a href="${process.env.BACKEND_URL}:${process.env.PORT ?? 3000}/out/recuperacion/${token}">Reestablecer Password</a>  </p>
+            <p>Sigue el siguiente enlace para generar un password nuevo: 
+            <a href="${process.env.BACKEND_URL}:${process.env.PORT ?? 3000}/auth/olvide-password/${token}">Reestablecer Password</a> </p>
 
-            <p> Si tu no solicitaste cambiar tu password esta cuenta puedes ignorar el mensaje  </p>
-            `
+            <p>Si tu no solicitaste el cambio de password, puedes ignorar el mensaje</p>
+        `
     })
 }
 
 
 export {
     emailRegistro,
-    emailRecuperacion
-
+    emailOlvidePassword
 }
