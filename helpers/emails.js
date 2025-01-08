920412dc8ba3ef8c3a29a7d545153a9d7213e201
Lisandro <lisansierra@outlook.com>
2025-01-08 09:55:11 -0400

    email pass tryed

--- helpers/emails.js
+++ helpers/emails.js
@@ -6,7 +6,7 @@ const emailRegistro = async (datos) => {
         port: process.env.EMAIL_PORT,
         auth: {
             user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASSWORD
+            pass: process.env.EMAIL_PASS
         }
     });
 
@@ -35,7 +35,7 @@ const emailOlvidePassword = async (datos) => {
         port: process.env.EMAIL_PORT,
         auth: {
             user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASSWORD
+            pass: process.env.EMAIL_PASS
         }
     });
 
