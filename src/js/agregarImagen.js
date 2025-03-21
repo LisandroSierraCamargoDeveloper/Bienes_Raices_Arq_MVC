338fbd71b0372385c1910d18ef8f2e42798f13bd
Lisandro <lisansierra@outlook.com>
2025-03-21 17:33:55 -0400

    Change in src agregarImagen

--- src/js/agregarImagen.js
+++ src/js/agregarImagen.js
@@ -1,34 +1,34 @@
-import  {Dropzone} from 'dropzone'
-const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+import { Dropzone } from 'dropzone'
 
-
+const token = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
 
 Dropzone.options.imagen = {
-    dictDefaultMessage:'Sube tus imagenes aqui',
-    acceptedFiles: '.png, .jpg, .jpeg',
+    dictDefaultMessage: 'Sube tus imágenes aquí',
+    acceptedFiles: '.png,.jpg,.jpeg',
     maxFilesize: 5,
-    maxFiles: 2,
-    parallelUploads:2,
-    autoProcessQueue:false,
-    addRemoveLinks:true,
-    dictRemoveFile:'Borrar Archivo',
-    dictMaxFilesExceeded: 'Limite de fotos alcanzado',
+    maxFiles: 1,
+    parallelUploads: 1,
+    autoProcessQueue: false,
+    addRemoveLinks: true,
+    dictRemoveFile: 'Borrar Archivo',
+    dictMaxFilesExceeded: 'El Limite es 1 Archivo',
     headers: {
-        'CSRF-Token':token
+        'CSRF-Token': token
     },
-    paramName:'imagen',
-    init: function(){
-         const dropzone = this
-         const btnPublcar = document.querySelector("#publicar")
+    paramName: 'imagen',
+    init: function() {
+        const dropzone = this
+        const btnPublicar = document.querySelector('#publicar')
 
-         btnPublcar.addEventListener('click', function(){
+        btnPublicar.addEventListener('click', function() {
             dropzone.processQueue()
-         })
+        })
 
-         dropzone.on('queuecomplete', function(){
-            if(dropzone.getActiveFiles().length === 0 ){
-                window.location.href = './mis-propiedades'
+        dropzone.on('queuecomplete', function() {
+            if(dropzone.getActiveFiles().length == 0) {
+                window.location.href = '/mis-propiedades'
             }
-         })
+        })
+
     }
 }
