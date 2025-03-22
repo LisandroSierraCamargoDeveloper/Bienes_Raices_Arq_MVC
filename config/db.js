05491e7d8af1728d7cda0f2a237b9bd74f30c96c
Lisandro <lisansierra@outlook.com>
2025-03-22 14:01:32 -0400

    arreglando conexion a base de datos

--- config/db.js
+++ config/db.js
@@ -1,22 +1,43 @@
-import Sequelize from 'sequelize'
-import dotenv from 'dotenv'
-dotenv.config({path: '.env'})
+import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
 
-const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASSWORD ?? '', {
+dotenv.config({ path: '.env' });
+
+const db = new Sequelize(
+  process.env.BD_NOMBRE,
+  process.env.BD_USER,
+  process.env.BD_PASS ?? '',
+  {
     host: process.env.BD_HOST,
-    port: process.env.BD_PORT,
+    port: process.env.BD_PORT || 3306, // Usa BD_PORT si está definido, sino 3306
     dialect: 'mysql',
     define: {
-        timestamps: true
+      timestamps: true,
     },
     pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }, 
-    operatorAliases: false
-});
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    dialectOptions: { // Important for render.
+      connectTimeout: 60000, // 60 seconds. Adjust as needed.
+    },
+    operatorAliases: false,
+  }
+);
 
+// Función para probar la conexión
+async function testConnection() {
+  try {
+    await db.authenticate();
+    console.log('Conexión a la base de datos establecida correctamente.');
+  } catch (error) {
+    console.error('Error al conectar a la base de datos:', error);
+  }
+}
+
+// Ejecuta la prueba de conexión al iniciar
+testConnection();
 
 export default db;
